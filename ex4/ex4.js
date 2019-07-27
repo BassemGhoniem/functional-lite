e2d04b859ee8f1c6439aa100b721f02374644151
BG <bassem.ghoniem@yahoo.com>
2019-07-28 00:19:03 +0200

    exercise 4 immutability

--- ex4/ex4.js
+++ ex4/ex4.js
@@ -2,12 +2,16 @@ function lotteryNum() {
 	return (Math.round(Math.random() * 100) % 58) + 1;
 }
 
-function pickNumber(){}
+function pickNumber(arr){
+	let num;
+	do { num = lotteryNum()	} while(~arr.indexOf(num))
+	return [num, ...arr].sort((x, y) => x - y);
+}
 
 var luckyLotteryNumbers = [];
 
 for (var i = 0; i < 6; i++) {
-	pickNumber();
+	luckyLotteryNumbers = pickNumber(Object.freeze(luckyLotteryNumbers));
 }
 
 console.log(luckyLotteryNumbers);
