aecee0ab1ea8108ece81b5fe50fdbd9137cb9390
BG <bassem.ghoniem@yahoo.com>
2019-07-27 23:41:08 +0200

    exercise 3 composition

--- ex3/ex3.js
+++ ex3/ex3.js
@@ -3,14 +3,24 @@ function decrement(x) { return x - 1; }
 function double(x) { return x * 2; }
 function half(x) { return x / 2; }
 
-function compose() {}
-function pipe() {}
+
+
+function compose(...args) {
+    return args.reduce((prev, curr) => {
+        return function(x) {
+            return prev(curr(x));
+        }
+    });
+}
+
+function pipe(...args) {
+    return compose(...args.reverse());
+}
 
 var f = compose(decrement,double,increment,half);
 var p = pipe(half,increment,double,decrement);
 
-f(3) === 4;
-// true
+console.log(f(3) === 4);
 
-f(3) === p(3);
+console.log(f(3) === p(3));
 // true
